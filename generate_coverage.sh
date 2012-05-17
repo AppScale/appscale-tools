fc8be78bbcd5c8b5b5208f0bfbc550fecfe6f202
Chris Bunch <shatterednirvana@gmail.com>
2012-05-16 23:30:23 -0700

    fixed generate_coverage script since we're now on github and don't need to do folder moving stuff, and regenerated coverage/rdoc

--- generate_coverage.sh
+++ generate_coverage.sh
@@ -1,10 +1,4 @@
 rm -rfv coverage
 
-cd ..
-mv tools-cgb-research tools
-cd tools
 # By default, we want to test coverage on the unit tests we have
 rcov test/ts_all.rb -x /usr/local/lib/site_ruby/1.8/rubygems/gem_path_searcher.rb
-cd ..
-mv tools tools-cgb-research
-cd tools-cgb-research
