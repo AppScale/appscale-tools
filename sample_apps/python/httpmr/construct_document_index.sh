ba55125b5048ad554ca9621bbe8d37aedb83d784
Chris Bunch <shatterednirvana@gmail.com>
2012-09-19 16:11:01 -0700

    removed sample apps, since we're moving it into its own repo

--- sample_apps/python/httpmr/construct_document_index.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-base_url=$1
-
-curl $base_url/load_fake_documents && \
-./httpmr/driver.py \
-  --httpmr_base=$base_url/construct_document_index \
-  --max_operations_inflight=10
-
--- sample_apps/python/sleep/sleep.py
+++
@@ -1,8 +0,0 @@
-import time
-starttime = time.time()
-time.sleep(5)
-endtime = time.time()
-print "Content-Type: text/plain"
-print ""
-print "StartTime:",str(starttime)
-print "EndTime:",str(endtime)
