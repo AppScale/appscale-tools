c6eea069f1a4fbeda8f1ffb9f7781bc589517ab4
Chris Bunch <shatterednirvana@gmail.com>
2012-12-13 07:50:05 -0800

    added initial method documentation

--- lib/appscale.py
+++ lib/appscale.py
@@ -3,4 +3,17 @@
 
 
 class AppScale():
-  pass
+
+
+  # Parses the arguments given to determine what command should be
+  # executed.
+  # Args:
+  #   args: A list of strs that correspond to the arguments passed to
+  #     the 'appscale' command.
+  # Returns:
+  #   A str corresponding to the directive that should be executed.
+  # Raises:
+  #   BadConfigurationError: If no directive was given, or if an
+  #     invalid directive was given.
+  def get_directive(args):
+    pass
