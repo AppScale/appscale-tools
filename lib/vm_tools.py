399733f800e860d5be852b58a85da15b70b312f6
Chris Bunch <shatterednirvana@gmail.com>
2013-01-22 15:14:53 -0800

    added skeleton vmtools file

---
+++ lib/vm_tools.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Programmer: Chris Bunch (chris@appscale.com)
+
+
+# A list of environment variables that the user must set
+# when deploying AppScale over a cloud infrastructure.
+EC2_ENVIRONMENT_VARIABLES = ["EC2_PRIVATE_KEY", "EC2_CERT",
+  "EC2_SECRET_KEY", "EC2_ACCESS_KEY"]
+
+
+class VMTools():
+  pass
