5637597d5687322f4ab34f6629d589fcce4e2d3b
Chris Bunch <shatterednirvana@gmail.com>
2013-01-29 11:13:56 -0800

    added in user_app_client from hiranya's branch

---
+++ lib/user_app_client.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Programmer: Chris Bunch (chris@appscale.com)
+# Adapted from Hiranya's version
+
+
+# Third-party imports
+import SOAPpy
+
+
+# AppScale-specific imports
+from appscale_logger import AppScaleLogger
+
+
+ADMIN_CAPABILITIES = ":".join(["upload_app", "mr_api", "ec2_api", "neptune_api"])
+
+
+class UserAppClient():
+
+
+  USER_APP_SERVER_PORT = 4343
+
+
+  def __init__(self, host, secret):
+    self.host = host
+    self.server = SOAPpy.SOAPProxy('https://%s:4343' % host)
+    self.secret = secret
+
+
+  def create_user(self, username, password, type='xmpp_user'):
+    AppScaleLogger.log("Creating new user account {0}".format(username)) 
+    result = self.server.commit_new_user(username, password, type, self.secret)
+    if result != 'true':
+      raise Exception(result)
+
+
+  def reserve_application_name(self, username, application, language):
+    AppScaleLogger.log("Registering application name {0} (lang={1}) for " + \
+      "user {2}".format(application, language, username))
+
+    result = self.server.commit_new_app(application, username,
+      language, self.secret)
+    if result != 'true':
+      raise Exception(result)
+
+
+  def commit_application_archive(self, application, file_path):
+    result = self.server.commit_tar(application, file_path, self.secret)
+    if result != 'true':
+      raise Exception(result)
+
+
+  def set_admin_role(self, username):
+    AppScaleLogger.log('Granting admin privileges to %s' % username)
+    self.server.set_cloud_admin_status(username, 'true', self.secret)
+    self.server.set_capabilities(username, ADMIN_CAPABILITIES, self.secret)
