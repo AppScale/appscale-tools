b45bf3f5369cd306f899adbbfeb7c71fe1dfcd33
Chris Bunch <shatterednirvana@gmail.com>
2013-04-08 07:50:35 -0700

    removed duplicate tests, cleaned up docs in upload_app tests

--- test/test_appscale_upload_app.py
+++ test/test_appscale_upload_app.py
@@ -110,8 +110,6 @@ class TestAppScaleUploadApp(unittest.TestCase):
 
 
   def test_upload_python25_app_with_no_appid(self):
-    # if the user gives us an app with a reserved appid, we should abort
-
     # add in mocks so that there is an app.yaml, but with no appid set
     flexmock(os.path)
     os.path.should_call('exists')
@@ -139,8 +137,6 @@ class TestAppScaleUploadApp(unittest.TestCase):
 
 
   def test_upload_java_app_with_no_appid(self):
-    # if the user gives us an app with a reserved appid, we should abort
-
     # add in mocks so that there is an appengine-web.xml, but with no appid set
     flexmock(os.path)
     os.path.should_call('exists')
@@ -230,9 +226,8 @@ class TestAppScaleUploadApp(unittest.TestCase):
     self.assertRaises(AppEngineConfigException, AppScaleTools.upload_app, options)
 
 
-  def test_upload_app_with_bad_runtime(self):
-    # we only support four runtimes - abort if the user gives us an unsupported
-    # one
+  def test_upload_app_with_reserved_app_id(self):
+    # users can't choose reserved appids for their own applications
 
     # add in mocks so that there is an app.yaml, but with no appid set
     flexmock(os.path)
@@ -262,9 +257,6 @@ class TestAppScaleUploadApp(unittest.TestCase):
 
 
   def test_upload_app_with_non_alpha_appid(self):
-    # we only support four runtimes - abort if the user gives us an unsupported
-    # one
-
     # add in mocks so that there is an app.yaml, but with no appid set
     flexmock(os.path)
     os.path.should_call('exists')
@@ -451,119 +443,6 @@ class TestAppScaleUploadApp(unittest.TestCase):
     self.assertRaises(AppScaleException, AppScaleTools.upload_app, options)
 
 
-  def test_upload_app_successfully(self):
-    remote_help = flexmock(RemoteHelper)
-    remote_help.should_receive('ssh').and_return()
-    remote_help.should_receive('scp').and_return()
-    remote_help.should_receive('copy_app_to_host').and_return()
-
-    # add in mocks so that there is an app.yaml, but with no appid set
-    flexmock(os.path)
-    os.path.should_call('exists')
-    app_yaml_location = AppEngineHelper.get_app_yaml_location(self.app_dir)
-    os.path.should_receive('exists').with_args(app_yaml_location) \
-      .and_return(True)
-
-    # mock out reading the app.yaml file
-    builtins = flexmock(sys.modules['__builtin__'])
-    builtins.should_call('open')  # set the fall-through
-
-    fake_app_yaml = flexmock(name="fake_app_yaml")
-    fake_app_yaml.should_receive('read').and_return(yaml.dump({
-      'application' : 'baz',
-      'runtime' : 'python'
-    }))
-    builtins.should_receive('open').with_args(app_yaml_location, 'r') \
-      .and_return(fake_app_yaml)
-
-    # mock out the SOAP call to the AppController and assume it succeeded
-    fake_appcontroller = flexmock(name='fake_appcontroller')
-    fake_appcontroller.should_receive('status').with_args('the secret') \
-      .and_return('Database is at public1')
-    fake_appcontroller.should_receive('done_uploading').with_args('baz',
-      '/var/apps/baz/app/baz.tar.gz', 'the secret').and_return()
-    fake_appcontroller.should_receive('update').with_args(['baz'],
-      'the secret').and_return()
-    fake_appcontroller.should_receive('is_app_running').with_args('baz',
-      'the secret').and_return(False).and_return(True)
-    flexmock(SOAPpy)
-    SOAPpy.should_receive('SOAPProxy').with_args('https://public1:17443') \
-      .and_return(fake_appcontroller)
-
-    # mock out reading the locations.json file, and slip in our own json
-    os.path.should_receive('exists').with_args(
-      LocalState.get_locations_json_location(self.keyname)).and_return(True)
-
-    fake_nodes_json = flexmock(name="fake_nodes_json")
-    fake_nodes_json.should_receive('read').and_return(json.dumps([{
-      "public_ip" : "public1",
-      "private_ip" : "private1",
-      "jobs" : ["shadow", "login"]
-    }]))
-    builtins.should_receive('open').with_args(
-      LocalState.get_locations_json_location(self.keyname), 'r') \
-      .and_return(fake_nodes_json)
-
-    # mock out reading the secret key from ~/.appscale
-    secret_key_location = LocalState.get_secret_key_location(self.keyname)
-    fake_secret = flexmock(name="fake_secret")
-    fake_secret.should_receive('read').and_return('the secret')
-    builtins.should_receive('open').with_args(secret_key_location, 'r') \
-      .and_return(fake_secret)
-
-    # mock out calls to the UserAppServer and presume that calls to create new
-    # users succeed
-    app_data = """
-    num_hosts:1
-    num_ports:1
-    hosts:public1
-    ports: 8080
-    """
-
-    fake_userappserver = flexmock(name='fake_userappserver')
-    fake_userappserver.should_receive('does_user_exist').with_args(
-      'a@a.com', 'the secret').and_return('false')
-    fake_userappserver.should_receive('commit_new_user').with_args(
-      'a@a.com', str, 'xmpp_user', 'the secret').and_return('true')
-    fake_userappserver.should_receive('commit_new_user').with_args(
-      'a@public1', str, 'xmpp_user', 'the secret').and_return('true')
-    fake_userappserver.should_receive('get_app_data').with_args(
-      'baz', 'the secret').and_return('\n\nnum_ports:0\n') \
-      .and_return(app_data).and_return(app_data).and_return(app_data)
-    fake_userappserver.should_receive('commit_new_app').with_args(
-      'baz', 'a@a.com', 'python', 'the secret').and_return('true')
-    SOAPpy.should_receive('SOAPProxy').with_args('https://public1:4343') \
-      .and_return(fake_userappserver)
-
-    # mock out asking the user for the admin on the new app, and slip in an
-    # answer for them
-    builtins.should_receive('raw_input').and_return("a@a.com")
-    flexmock(getpass)
-    getpass.should_receive('getpass').and_return('aaaaaa')
-
-
-    # as well as removing the tar'ed app once we're done copying it
-    flexmock(os)
-    os.should_receive('remove').with_args('/tmp/appscale-app-baz.tar.gz') \
-      .and_return()
-
-    # and slap in a mock that says the app comes up after waiting for it
-    # three times
-    fake_socket = flexmock(name='fake_socket')
-    fake_socket.should_receive('connect').with_args(('public1',
-      8080)).and_raise(Exception).and_raise(Exception) \
-      .and_return(None)
-    flexmock(socket)
-    socket.should_receive('socket').and_return(fake_socket)
-
-    argv = [
-      "--keyname", self.keyname,
-      "--file", self.app_dir
-    ]
-    options = ParseArgs(argv, self.function).args
-    AppScaleTools.upload_app(options)
-  
-
   def test_upload_app_when_app_exists_on_cloud(self):
     # we don't let you upload an app if it's already running, so abort
 
@@ -626,86 +505,6 @@ class TestAppScaleUploadApp(unittest.TestCase):
       'a@public1', str, 'xmpp_user', 'the secret').and_return('true')
     fake_userappserver.should_receive('get_app_data').with_args(
       'baz', 'the secret').and_return("\nnum_ports:10\n")
-    SOAPpy.should_receive('SOAPProxy').with_args('https://public1:4343') \
-      .and_return(fake_userappserver)
-
-    # mock out asking the user for the admin on the new app, and slip in an
-    # answer for them
-    builtins.should_receive('raw_input').and_return("a@a.com")
-    flexmock(getpass)
-    getpass.should_receive('getpass').and_return('aaaaaa')
-
-    argv = [
-      "--keyname", self.keyname,
-      "--file", self.app_dir
-    ]
-    options = ParseArgs(argv, self.function).args
-    self.assertRaises(AppScaleException, AppScaleTools.upload_app, options)
-
-
-  def test_upload_app_when_app_admin_not_this_user(self):
-    # we don't let you upload an app if the appid is registered to someone else,
-    # so abort
-
-    # add in mocks so that there is an app.yaml, but with no appid set
-    flexmock(os.path)
-    os.path.should_call('exists')
-    app_yaml_location = AppEngineHelper.get_app_yaml_location(self.app_dir)
-    os.path.should_receive('exists').with_args(app_yaml_location) \
-      .and_return(True)
-
-    # mock out reading the app.yaml file
-    builtins = flexmock(sys.modules['__builtin__'])
-    builtins.should_call('open')  # set the fall-through
-
-    fake_app_yaml = flexmock(name="fake_app_yaml")
-    fake_app_yaml.should_receive('read').and_return(yaml.dump({
-      'application' : 'baz',
-      'runtime' : 'python'
-    }))
-    builtins.should_receive('open').with_args(app_yaml_location, 'r') \
-      .and_return(fake_app_yaml)
-
-    # mock out the SOAP call to the AppController and assume it succeeded
-    fake_appcontroller = flexmock(name='fake_appcontroller')
-    fake_appcontroller.should_receive('status').with_args('the secret') \
-      .and_return('Database is at public1')
-    flexmock(SOAPpy)
-    SOAPpy.should_receive('SOAPProxy').with_args('https://public1:17443') \
-      .and_return(fake_appcontroller)
-
-    # mock out reading the locations.json file, and slip in our own json
-    os.path.should_receive('exists').with_args(
-      LocalState.get_locations_json_location(self.keyname)).and_return(True)
-
-    fake_nodes_json = flexmock(name="fake_nodes_json")
-    fake_nodes_json.should_receive('read').and_return(json.dumps([{
-      "public_ip" : "public1",
-      "private_ip" : "private1",
-      "jobs" : ["shadow", "login"]
-    }]))
-    builtins.should_receive('open').with_args(
-      LocalState.get_locations_json_location(self.keyname), 'r') \
-      .and_return(fake_nodes_json)
-
-    # mock out reading the secret key from ~/.appscale
-    secret_key_location = LocalState.get_secret_key_location(self.keyname)
-    fake_secret = flexmock(name="fake_secret")
-    fake_secret.should_receive('read').and_return('the secret')
-    builtins.should_receive('open').with_args(secret_key_location, 'r') \
-      .and_return(fake_secret)
-
-    # mock out calls to the UserAppServer and presume that calls to create new
-    # users succeed
-    fake_userappserver = flexmock(name='fake_userappserver')
-    fake_userappserver.should_receive('does_user_exist').with_args(
-      'a@a.com', 'the secret').and_return('false')
-    fake_userappserver.should_receive('commit_new_user').with_args(
-      'a@a.com', str, 'xmpp_user', 'the secret').and_return('true')
-    fake_userappserver.should_receive('commit_new_user').with_args(
-      'a@public1', str, 'xmpp_user', 'the secret').and_return('true')
-    fake_userappserver.should_receive('get_app_data').with_args(
-      'baz', 'the secret').and_return('\n\nnum_ports:0\n\napp_owner:b@b.com')
     SOAPpy.should_receive('SOAPProxy').with_args('https://public1:4343') \
       .and_return(fake_userappserver)
 
