d2245dca8ba631970f938ad7dacfbffeb119b4cf
Chris Bunch <shatterednirvana@gmail.com>
2013-05-30 13:41:57 -0700

    added unit tests for version helper

--- lib/version_helper.py
+++ lib/version_helper.py
@@ -14,10 +14,10 @@ import sys
 
 
 # Third-party Python libraries
-from termcolor import cprint
+import termcolor
 
 
-def ensure_valid_python_is_used():
+def ensure_valid_python_is_used(system=sys):
   """ Makes sure that we are running a version of Python that the AppScale
   Tools supports.
 
@@ -26,17 +26,21 @@ def ensure_valid_python_is_used():
   the inevitable syntax errors that occur on lines like 'except Exception as e',
   which aren't Python 2.5-friendly.
 
+  Args:
+    system: A reference to the sys module. We add this in as an argument to make
+      it easy to mock out (as mocking out it or hasattr and tuple purely with
+      flexmock is difficult).
   Raises:
     SystemExit: If the version of Python that is running the AppScale Tools is
       older than 2.6.
   """
-  if not hasattr(sys, 'version_info'):
-    cprint("Very old versions of Python are not supported. Please "
+  if not hasattr(system, 'version_info'):
+    termcolor.cprint("Very old versions of Python are not supported. Please "
       "use version 2.6 or newer.\n", "red")
     sys.exit(1)
 
-  version_tuple = tuple(sys.version_info[:2])
+  version_tuple = tuple(system.version_info[:2])
   if version_tuple < (2, 6):
-    cprint("Error: Python %d.%d is not supported. Please use "
+    termcolor.cprint("Error: Python %d.%d is not supported. Please use "
       "version 2.6 or newer.\n" % version_tuple, "red")
     sys.exit(1)
