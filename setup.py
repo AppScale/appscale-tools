d933aa440081104440e0082790895b7ce30c0ff2
Chris Donati <chris.donati@rivdata.com>
2014-10-07 11:52:33 -0700

    Add setup.py for distributing tools as a package

---
+++ setup.py
@@ -0,0 +1,41 @@
+from lib import version_helper
+from setuptools import setup
+import glob
+
+version_helper.ensure_valid_python_is_used()
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='appscale-tools',
+      version='2.0.0',
+      description='A set of command-line tools for interacting with AppScale',
+      long_description=readme(),
+      author='AppScale Systems, Inc.',
+      url='https://github.com/AppScale/appscale-tools',
+      license='Apache License 2.0',
+      keywords='appscale google-app-engine python java go php',
+      platforms='Posix; MacOS X',
+      install_requires=['httplib2',
+                        'termcolor',
+                        'SOAPpy',
+                        'PyYAML',
+                        'boto',
+                        'google-api-python-client',
+                        'argparse',
+                        'python-gflags'],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Utilities'],
+      package_dir={'appscale': 'lib'},
+      packages=['appscale', 'appscale.agents'],
+      scripts=glob.glob('bin/*'),
+      package_data={'appscale': ['../templates/*']},
+     )
