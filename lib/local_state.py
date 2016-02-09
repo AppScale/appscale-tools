169e12cb3dcaf7f4b2d48fa1c282ebe492f0750f
menivaitsi <melpomeni.vaitsi@appscale.com>
2016-02-09 10:40:40 -0800

    Version bumped to 2.7.0

--- lib/local_state.py
+++ lib/local_state.py
@@ -28,7 +28,7 @@ from custom_exceptions import ShellException
 
 
 # The version of the AppScale Tools we're running on.
-APPSCALE_VERSION = "2.6.0"
+APPSCALE_VERSION = "2.7.0"
 
 
 class LocalState(object):
