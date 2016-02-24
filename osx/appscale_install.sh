036b3241e756a1b30943d5227f307c6c1b612cf6
Chris Donati <chris.donati@appscale.com>
2016-02-24 09:19:01 -0800

    Upgrade python packages if possible during osx install

--- osx/appscale_install.sh
+++ osx/appscale_install.sh
@@ -8,4 +8,4 @@ mkdir -p $TARGETDIR
 cp -r bin lib templates LICENSE README.md $TARGETDIR || exit 1
 
 # from installpylibs
-easy_install termcolor SOAPpy pyyaml boto argparse oauth2client google-api-python-client httplib2
+easy_install --upgrade termcolor SOAPpy pyyaml boto argparse oauth2client google-api-python-client httplib2
