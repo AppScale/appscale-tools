b28e97126e7f8c140404f394cc32b8234e1ed195
Chris Donati <chris.donati@appscale.com>
2016-05-04 18:58:04 -0700

    Simplify build script.
    We can add the debian packaging code back if we need it later.

--- debian/appscale_build.sh
+++ debian/appscale_build.sh
@@ -4,31 +4,20 @@ export DIST=`lsb_release -c -s`
 
 cd `dirname $0`/..
 
-if [ "$DIST" = "n/a" ] ||  [ ! -e ./debian/appscale_install_${DIST}.sh ]; then
-    echo "We do not have a build script for ${DIST}."
-    echo "You can still install this package from source with 'python setup.py install'."
-    exit 1
-fi
-
-echo "Installing Ubuntu ${DIST} building environment."
+echo "Installing AppScale tools on ${DIST}."
 
 # Install the deb packages specified for this distro.
-PACKAGES=`find debian -regex ".*\/control\.${DIST}\$" -exec mawk -f debian/package-list.awk {} +`
-apt-get update
-apt-get install -y --force-yes ${PACKAGES}
-if [ $? -ne 0 ]; then
-    echo "Failed to install deb packages for ${DIST}."
-    exit 1
+if [ -f ./debian/control.${DIST} ]; then
+    PACKAGES=$(find debian -regex ".*\/control\.${DIST}\$" -exec mawk -f debian/package-list.awk {} +)
+    apt-get update
+    apt-get install -y --force-yes ${PACKAGES}
+    if [ $? -ne 0 ]; then
+        echo "Failed to install deb packages for ${DIST}."
+        exit 1
+    fi
 fi
 
-# copy tools files
-TARGETDIR=/usr/local/appscale-tools
-mkdir -p $TARGETDIR
-cp -rv bin lib templates $TARGETDIR || exit 1
-cp -v LICENSE README.md $TARGETDIR || exit 1
-
-# install scripts
-bash debian/appscale_install_${DIST}.sh all
+python2 setup.py install
 if [ $? -ne 0 ]; then
     echo "Unable to complete AppScale tools installation."
     exit 1
