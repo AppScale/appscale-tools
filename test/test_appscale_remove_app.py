5d0d04e91eea34d4b9e9ef436f3afe0269e7e7c9
Graziano Obertelli <graziano@appscale.com>
2016-07-20 00:34:11 -0700

    use is_app_running in unit test.

--- test/test_appscale_remove_app.py
+++ test/test_appscale_remove_app.py
@@ -70,7 +70,7 @@ class TestAppScaleRemoveApp(unittest.TestCase):
     fake_appcontroller = flexmock(name='fake_appcontroller')
     fake_appcontroller.should_receive('status').with_args('the secret') \
       .and_return('Database is at public1')
-    fake_appcontroller.should_receive('does_app_exist').with_args('blargapp',
+    fake_appcontroller.should_receive('is_app_running').with_args('blargapp',
       'the secret').and_return(False)
     flexmock(SOAPpy)
     SOAPpy.should_receive('SOAPProxy').with_args('https://public1:17443') \
