e489f26988b38a9240ed14856f486afde5439c6a
whoarethebritons <eden.akins@gmail.com>
2016-11-18 13:40:27 -0800

    fixed syntax of returning default empty list

--- appscale/tools/local_state.py
+++ appscale/tools/local_state.py
@@ -405,7 +405,7 @@ class LocalState(object):
     try:
       with open(cls.get_locations_json_location(keyname), 'r') as file_handle:
         return json.loads(file_handle.read()).get('infrastructure_info',
-                                                  default={}).get(tag, [])
+                                                  {}).get(tag, [])
     except IOError:
       raise BadConfigurationException("Couldn't read from locations file, "
                                       "AppScale may not be running with "
