469aab8bcf1bb4e5b477ed2883b4c6bf9ebfd582
Tanvi Marballi <tanvimarballi@gmail.com>
2017-02-07 15:24:35 -0800

    Add a way to clean up the left over keyname files.

--- appscale/tools/local_state.py
+++ appscale/tools/local_state.py
@@ -355,6 +355,18 @@ class LocalState(object):
     """
     return cls.LOCAL_APPSCALE_PATH + "locations-" + keyname + ".json"
 
+  @classmethod
+  def cleanup_keyname(cls, keyname):
+    """Cleans up all the files starting with the keyname upon termination
+    of cloud instances.
+
+    Args:
+        keyname: A str that indicates the name of the SSH keypair that
+          uniquely identifies this AppScale deployment.
+    """
+    file_path = cls.LOCAL_APPSCALE_PATH + keyname + "*"
+    for keyname_file in glob.glob(file_path):
+      os.remove(keyname_file)
 
   @classmethod
   def update_local_metadata(cls, options, db_master, head_node):
