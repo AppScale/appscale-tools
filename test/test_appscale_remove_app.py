ce20cf43abd479b7ae218567b60e9a4ffaa1cb90
Graziano Obertelli <graziano@appscale.com>
2017-02-07 17:47:34 -0800

    Fix unit test

--- test/test_appscale_remove_app.py
+++ test/test_appscale_remove_app.py
@@ -60,6 +60,8 @@ class TestAppScaleRemoveApp(unittest.TestCase):
     # mock out reading the secret key
     builtins.should_call('open')  # set the fall-through
 
+    app_stats_data = {'apps': {'pippo': {'http': 8080, 'language': 'python27',
+      'total_reqs': 'no_change', 'appservers': 1, 'https': 4380, 'reqs_enqueued': None}}}
     secret_key_location = LocalState.get_secret_key_location(self.keyname)
     fake_secret = flexmock(name="fake_secret")
     fake_secret.should_receive('read').and_return('the secret')
@@ -70,6 +72,8 @@ class TestAppScaleRemoveApp(unittest.TestCase):
     fake_appcontroller = flexmock(name='fake_appcontroller')
     fake_appcontroller.should_receive('status').with_args('the secret') \
       .and_return('Database is at public1')
+    fake_appcontroller.should_receive('get_all_stats').with_args(
+      'the secret').and_return(json.dumps(app_stats_data))
     flexmock(SOAPpy)
     SOAPpy.should_receive('SOAPProxy').with_args('https://public1:17443') \
       .and_return(fake_appcontroller)
