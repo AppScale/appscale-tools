7af59d8e182560add5399973ee659125700e7ea1
whoarethebritons <eden.akins@gmail.com>
2018-09-07 00:46:27 -0700

    a very important typo

--- appscale/tools/agents/azure_agent.py
+++ appscale/tools/agents/azure_agent.py
@@ -1015,7 +1015,7 @@ class AzureAgent(BaseAgent):
       vmss_delete_exceptions = []
 
       try:
-        ss_to_delete = [vmss.name for vmss in vmss_list if
+        ss_to_delete = [vmss.name for vmss in vmss_list if not
           any(compute_client.virtual_machine_scale_set_vms.list(resource_group, vmss.name))]
       except CloudError as e:
         logging.exception("CloudError received while trying to terminate "
